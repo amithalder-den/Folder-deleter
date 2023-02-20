7d7fe405dfe42a883d5a3271938a6249df02c5dd
amithalder-den <118268625+amithalder-den@users.noreply.github.com>
2023-02-20 15:30:23 +0530

    Update final.sh
    Changed log name variable

--- final.sh
+++ final.sh
@@ -76,7 +76,7 @@ while read L1 <&3 && read L2 <&4; do
 done 3<organization_account.txt 4<organization_repo.txt
 EOF
 chmod +x folder-remover.sh
-dt=`date '+%d-%m-%Y_%H:%M:%S'`
+dt=`date '+%d-%m-%Y_%H-%M-%S'`
 sh folder-remover.sh organization_account.txt organization_repo.txt > folder-deletion-log-$dt.out
 
 echo "##Important file paths###############"
